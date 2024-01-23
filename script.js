d8f1255d615857e399ba49961311ca34350f87d8
Yanis Renaud <59774973+LeganV9@users.noreply.github.com>
2024-01-23 01:22:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-alert("Hello! Welcome on Positivum, the first site where you chose your actual mood and get a positive quote to have a good day!");
 
+    <script>
+// Alerte pour bienvenue sur page
+      alert("Hello! Welcome on Positivum, the first site where you chose your actual mood and get a positive quote to have a good day!");
+
+// Utiliser fetch pour charger le contenu de header.html
+        fetch('header.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('headerContainer').innerHTML = data;
+            });
+    </script>
