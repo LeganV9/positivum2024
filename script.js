33911410c95c30aa96d5860257b877fbdef1d2f4
Yanis Renaud <59774973+LeganV9@users.noreply.github.com>
2024-03-05 19:53:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ const listePhrasesBad = [
   "Success is falling nine times and getting up ten. - Jon Bon Jovi",
 ];
 
+const listePhrasesNeutral = [
+  "Challenges are what make life interesting. Overcoming them is what makes life meaningful. - Joshua J. Marine",
+  "The only place where success comes before work is in the dictionary. - Vidal Sassoon",
+  "The greatest failure is the failure to try. - Chinese Proverb",
+  "Your only limit is yourself. - Arnold Schwarzenegger",
+  "Failures are the pillars of success. - Charles Chaplin",
+  "The only way to do great work is to love what you do. - Steve Jobs",
+  "Difficulty attracts the man of character because it is by tackling it that he realizes himself. - Johann Wolfgang von Goethe",
+  "Every failure brings you closer to success. - Jack Canfield",
+  "We can't fail without our consent. - Eleanor Roosevelt",
+  "Success is falling nine times and getting up ten. - Jon Bon Jovi",
+];
+
 function afficherPhraseAleatoire() {
   const nombreAleatoire = Math.floor(Math.random() * listePhrasesBad.length);
   const phraseAleatoire = listePhrasesBad[nombreAleatoire];
